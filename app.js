0b7b86e53fb686388129790ce4c12aaf0e611c06
maheshoz <35638589+maheshoz@users.noreply.github.com>
2018-04-01 18:26:07 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+ 
+ // Initialize Firebase
+	  var config = {
+	    apiKey: "AIzaSyCfmUV8apnro0ZY4LbwxnzxNM6NpTBSf00",
+	    authDomain: "vot-app.firebaseapp.com",
+	    databaseURL: "https://vot-app.firebaseio.com",
+	    projectId: "vot-app",
+	    storageBucket: "",
+	    messagingSenderId: "616079849230"
+	  };
+	  firebase.initializeApp(config);
